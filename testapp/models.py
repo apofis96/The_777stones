f8bc7ee06eccc9dd0ae02c25c0ca37db66d45364
Маша Саранцева <syanelse@gmail.com>
2018-04-20 18:49:00 +0300

    Created DB

--- testapp/models.py
+++ testapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class Players(models.Model):
+    username = models.CharField(max_length=20)
+    # можливості для розширення..
+
+class Games(models.Model):
+    isCompleted = models.BooleanField()
+    isPublic = models.BooleanField()
+    OwnerID = models.IntegerField()
+    secondPlayerID = models.IntegerField()
+    winnerID = models.IntegerField()
+
+class GameMoves(models.Model):
+    gameID = models.IntegerField()
+    moveNo = models.IntegerField()
+    # 'r' == rock(stone), 's' == scissors, 'p' == paper
+    ownerMove = models.CharField(max_length=1)
+    secondPlayerMove = models.CharField(max_length=1)
+
+
